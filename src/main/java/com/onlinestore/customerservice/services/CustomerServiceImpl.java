9ef53d6aac57f6dc5e701a845a29694fcbf10fb2
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-11 12:27:38 +0100

    added customerRepository to customerService

--- src/main/java/com/onlinestore/customerservice/services/CustomerServiceImpl.java
+++ src/main/java/com/onlinestore/customerservice/services/CustomerServiceImpl.java
@@ -2,15 +2,21 @@ package com.onlinestore.customerservice.services;
 
 import com.onlinestore.customerservice.model.Customer;
 import com.onlinestore.customerservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-CustomerRepository customerRepository;
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
 	public Customer getCustomerById(String customerId) {
-		return null;
+		return customerRepository.findByCustomerId(customerId);
 	}
 }
