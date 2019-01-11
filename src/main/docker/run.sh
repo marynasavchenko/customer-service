04cbfe24e2f9f6854ec79a21eb1d7e96f8278bd4
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-11 15:50:37 +0100

    changed bootstrap.yml

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -12,6 +12,6 @@ while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
 
 echo "********************************************************"
-echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "Starting Customer Server with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/customerservice/@project.build.finalName@.jar
