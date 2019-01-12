2a4eaac618a9f5b930057d03fd9860536374555a
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-12 19:40:31 +0100

    added POSTGRES_HOST

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -2,7 +2,7 @@
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
+while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
 echo "********************************************************"
@@ -12,6 +12,7 @@ while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
 
 echo "********************************************************"
-echo "Starting Customer Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "Starting Customer Server with Configuration Service : $CONFIGSERVER_URI";
 echo "********************************************************"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/customerservice/@project.build.finalName@.jar
+
