b0f53123638c2372232e59ac16b3b5d704477afb
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-01 15:59:02 +0100

    changed to JpaRepository

--- src/main/java/com/onlinestore/customerservice/repositories/CustomerRepository.java
+++ src/main/java/com/onlinestore/customerservice/repositories/CustomerRepository.java
@@ -1,10 +1,10 @@
 package com.onlinestore.customerservice.repositories;
 
 import com.onlinestore.customerservice.model.Customer;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, String> {
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 	Customer findByCustomerId(String customerId);
 }
