a34fbce4a83bed14ad0f90fae65bed086e51e693
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 08:48:23 +0100

    added doc comments to CustomerApplication

--- src/main/java/com/onlinestore/customerservice/CustomerApplication.java
+++ src/main/java/com/onlinestore/customerservice/CustomerApplication.java
@@ -3,8 +3,16 @@ package com.onlinestore.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class that bootstraps this service.
+ */
 @SpringBootApplication
 public class CustomerApplication {
+	/**
+	 * Main method, used to run this application.
+	 *
+	 * @param args the string array, that contains command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
