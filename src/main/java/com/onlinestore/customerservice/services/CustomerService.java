83f11de6a17a04f467ddb83a6dd9da708806b6d5
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 09:15:25 +0100

    added doc comments to CustomerService

--- src/main/java/com/onlinestore/customerservice/services/CustomerService.java
+++ src/main/java/com/onlinestore/customerservice/services/CustomerService.java
@@ -2,7 +2,22 @@ package com.onlinestore.customerservice.services;
 
 import com.onlinestore.customerservice.domain.Customer;
 
+/**
+ * {@link CustomerService} defines an interface that supports operations for managing customers.
+ */
 public interface CustomerService {
+	/**
+	 * Gets customer by specified id.
+	 *
+	 * @param customerId unique Id of the customer
+	 * @return customer
+	 */
 	Customer getCustomerById(String customerId);
+
+	/**
+	 * Saves new customer.
+	 *
+	 * @param customer
+	 */
 	void saveCustomer(Customer customer);
 }
