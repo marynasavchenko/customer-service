ce09de715f926ee45c62e40f65757c9a0acd8f1d
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 08:53:27 +0100

    added doc comments to CustomerController

--- src/main/java/com/onlinestore/customerservice/controllers/CustomerController.java
+++ src/main/java/com/onlinestore/customerservice/controllers/CustomerController.java
@@ -8,17 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest controller for managing customers information.
+ */
 @RestController
 @RequestMapping("/v1/customers")
 public class CustomerController {
-
+	/**
+	 * Service that supports operations with customers.
+	 */
 	private CustomerService customerService;
 
+	/**
+	 * Constructs a new {@code OrderController} instance.
+	 *
+	 * @param customerService service that supports operations with customers
+	 */
 	@Autowired
 	public CustomerController(CustomerService customerService) {
 		this.customerService = customerService;
 	}
 
+	/**
+	 * GET  / : get a customer by id.
+	 *
+	 * @param customerId the unique id of the customer
+	 * @return a customer
+	 */
 	@GetMapping("/{customerId}")
 	public Customer getCustomerById(@PathVariable("customerId") String customerId) {
 		return customerService.getCustomerById(customerId);
