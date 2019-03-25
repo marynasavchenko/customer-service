f02ebeff26055abb02b037c9f0f3954bce192c97
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 09:45:42 +0100

    added doc comments to CustomerServiceImpl

--- src/main/java/com/onlinestore/customerservice/services/CustomerServiceImpl.java
+++ src/main/java/com/onlinestore/customerservice/services/CustomerServiceImpl.java
@@ -5,21 +5,42 @@ import com.onlinestore.customerservice.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementation of the {@link CustomerService} interface.
+ */
 @Service
 public class CustomerServiceImpl implements CustomerService {
-
+	/**
+	 * Spring Data repository for customers.
+	 */
 	private CustomerRepository customerRepository;
 
+	/**
+	 * Constructs new {@code CustomerServiceImpl} instance.
+	 *
+	 * @param customerRepository Spring Data repository for customers
+	 */
 	@Autowired
 	public CustomerServiceImpl(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
 
+	/**
+	 * Gets customer by id.
+	 *
+	 * @param customerId unique Id of the customer
+	 * @return customer
+	 */
 	@Override
 	public Customer getCustomerById(String customerId) {
 		return customerRepository.findByCustomerId(customerId);
 	}
 
+	/**
+	 * Saves new customer.
+	 *
+	 * @param customer
+	 */
 	@Override
 	public void saveCustomer(Customer customer) {
 		customerRepository.save(customer);
