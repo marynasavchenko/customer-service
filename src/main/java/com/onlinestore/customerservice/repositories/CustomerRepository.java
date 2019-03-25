6883a655f088ab0fab2e1ba2dbc487d59f1202d4
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 09:08:58 +0100

    added doc comments to CustomerRepository

--- src/main/java/com/onlinestore/customerservice/repositories/CustomerRepository.java
+++ src/main/java/com/onlinestore/customerservice/repositories/CustomerRepository.java
@@ -4,7 +4,16 @@ import com.onlinestore.customerservice.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Spring Data JPA repository for Customer entity that supports CRUD operations.
+ */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+	/**
+	 * Looks up customer in the database.
+	 *
+	 * @param customerId unique Id of the customer
+	 * @return customer
+	 */
 	Customer findByCustomerId(String customerId);
 }
