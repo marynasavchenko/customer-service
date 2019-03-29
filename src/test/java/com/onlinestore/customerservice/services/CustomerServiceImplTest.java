67c9ef8bc2c67b36b4343f0d64b8d99fb3f14dc8
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-29 14:29:24 +0100

    added CustomerServiceImplTest

---
+++ src/test/java/com/onlinestore/customerservice/services/CustomerServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.onlinestore.customerservice.services;
+
+import com.onlinestore.customerservice.domain.Customer;
+import com.onlinestore.customerservice.repositories.CustomerRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerServiceImplTest {
+	private static final String ANY_CUSTOMER_ID = "1234";
+
+	@Mock
+	private CustomerRepository customerRepository;
+
+	@Mock
+	private Customer customer;
+
+	private CustomerService customerService;
+
+	@Before
+	public void setUp() throws Exception {
+		customerService = new CustomerServiceImpl(customerRepository);
+	}
+
+	@Test
+	public void shouldGetCustomerById() throws Exception {
+		when(customerRepository.findByCustomerId(ANY_CUSTOMER_ID)).thenReturn(Optional.of(customer));
+		customerService.getCustomerById(ANY_CUSTOMER_ID);
+		verify(customerRepository).findByCustomerId(ANY_CUSTOMER_ID);
+	}
+}
