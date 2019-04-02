028fcacaf71a185fdf8f635df11d9e489d1f6a3a
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-02 10:08:44 +0200

    added constructor to CustomerNotFoundException

--- src/main/java/com/onlinestore/customerservice/exceptions/CustomerNotFoundException.java
+++ src/main/java/com/onlinestore/customerservice/exceptions/CustomerNotFoundException.java
@@ -4,4 +4,7 @@ package com.onlinestore.customerservice.exceptions;
  * Exception thrown when customer can not be found.
  */
 public class CustomerNotFoundException extends RuntimeException {
+	public CustomerNotFoundException(String customerId) {
+		super("Customer not found" + customerId);
+	}
 }
