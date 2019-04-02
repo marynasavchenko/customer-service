ae7f0cb518e85ae966fbb2bdd93e50868aa55ac9
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-02 11:19:50 +0200

    added doc comments

--- src/main/java/com/onlinestore/customerservice/exceptions/CustomerNotFoundException.java
+++ src/main/java/com/onlinestore/customerservice/exceptions/CustomerNotFoundException.java
@@ -4,6 +4,11 @@ package com.onlinestore.customerservice.exceptions;
  * Exception thrown when customer can not be found.
  */
 public class CustomerNotFoundException extends RuntimeException {
+	/**
+	 * Constructs a new CustomerNotFoundException instance.
+	 *
+	 * @param customerId unique Id of the customer.
+	 */
 	public CustomerNotFoundException(String customerId) {
 		super("Customer not found" + customerId);
 	}
