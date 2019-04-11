c6ef23d1e49932e07127cd6a760d48bbdaa15dfe
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-11 14:44:40 +0200

    added shouldAddNewCustomer()

--- src/test/java/com/onlinestore/customerservice/controllers/CustomerControllerTest.java
+++ src/test/java/com/onlinestore/customerservice/controllers/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package com.onlinestore.customerservice.controllers;
 
+import com.onlinestore.customerservice.domain.Customer;
 import com.onlinestore.customerservice.services.CustomerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -28,5 +31,11 @@ public class CustomerControllerTest {
 	public void shouldGetCustomerById() throws Exception {
 		mockMvc.perform(get("/v1/customers/"+CUSTOMER_ID)).andExpect(status().isOk());
 		verify(customerService).getCustomerById(CUSTOMER_ID);
+	}
+
+	@Test
+	public void shouldAddNewCustomer() throws Exception {
+		mockMvc.perform(post("/v1/customers/")).andExpect(status().isOk());
+		verify(customerService).saveCustomer(any(Customer.class));
 	}
 }
